b952f7da7dcddf4b612a62f3baa53d3db913f532
Clement Ching <clement.ching@zetaris.com>
2023-09-07 09:33:25 +1000

    add gitignore installation commands and nfs values

--- mainchart/installation_commands.sh
+++ mainchart/installation_commands.sh
@@ -1,3 +1,11 @@
+namespace="{yournamespace}"
+
+#install kubectl and helm if needed
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash 
+
 #ingress nginx
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade --install lightning-ingress ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --version 4.0.18 --set rbac.create=true --set rbac.createRole=true --set rbac.createClusterRole=true --set-string controller.config.proxy-body-size=100m --set-string controller.config.server-tokens=false --set controller.config.generate-request-id=\"false\" 
@@ -19,29 +27,14 @@ openssl pkcs8 -topk8 -inform PEM -outform DER -in private_key.pem -out private_k
 openssl rsa -in private_key.pem -pubout -outform DER -out public_key.der
 
 #create namespace and secret
-kubectl create namespace {yournamespace}
+kubectl create namespace $namespace
 kubectl create secret generic lightning-password-security-encryption-secret
---from-file=private_key.der --from-file=public_key.der –-namespace {yournamespace}
+--from-file=private_key.der --from-file=public_key.der –-namespace $namespace
 
-#For Tanzu, create NFS
-nfs:
-  # The NFS server endpoint that we got from vSAN FS
-  server: vsan-fs01.tanzu.lab
-  # The NFS share mount path from vSAN FS
-  path: /share01
-storageClass:
-  # The name of the StorageClass to be created on the K8s cluster to allow provisioning of RWM volumes from the share
-  name: nfs-external
-  # The accessMode that we want to be created from these subvolumes (ReadWriteMany allows multiple containers to mount it at once)
-  accessModes: ReadWriteMany
-podSecurityPolicy:
-# Enabling the pod security policy allows it to run on TKGS clusters out of the box
-  enabled: true
-
+#Install NFS
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
 helm repo update
-kubectl create namespace infra
-helm install nfs-subdir-external-provisioner --namespace infra nfs-subdir-external-provisioner/nfs-subdir-external-provisioner -f nfsvalues.yaml
+helm install nfs-subdir-external-provisioner --namespace $namespace nfs-subdir-external-provisioner/nfs-subdir-external-provisioner -f nfsvalues.yaml
 
 ## install Zetaris => Modify the main chart/zetaris/values file
 #use Dry run to validate deployment
@@ -49,3 +42,8 @@ helm upgrade --install --dry-run zetaris mainchart/zetaris --namespace zetaris -
 #install Zetaris
 helm upgrade --install zetaris mainchart/zetaris --namespace zetaris -f ./mainchart/zetaris/values.yaml
 
+
+#Install Airflow
+helm repo add apache-airflow https://airflow.apache.org
+#modify airflow.values.yaml then install
+helm upgrade --install airflow apache-airflow/airflow -f ./airflowvalues.yaml --namespace airflow --create-namespace
