cff63816c01fa263010f0dbdf510ed0bc795ca73
Ganeshbabu Chandran <ganeshbabuchandran@Ganeshbabus-MacBook-Pro.local>
2025-02-11 11:02:38 +0530

    tagging main branch with v2.3.6.0-vmware

--- imagetag.sh
+++ imagetag.sh
@@ -1,10 +1,10 @@
-branchname="v2.3.6.0-tanzuvmware"
+branchname="v2.3.6.0-vmware"
 ghtoken="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"
 #### Package helm charts
 helm package ./mainchart/zetaris -d ./mainchart/zetaris
 
 #### Adding index
-helm repo index mainchart/zetaris/ --url https://$ghtoken@raw.githubusercontent.com/zetaris/HelmDeployment/$branchname/mainchart/zetaris
+helm repo index mainchart/zetaris/ --url https://$ghtoken@raw.githubusercontent.com/zetaris/TanzuInstallation/$branchname/mainchart/zetaris
 
 git add .
 git commit -a -m "tagging main branch with $branchname"
